fed47e079f8017a5a1ead82c9b9a1ddbdb11c6f0
thaomike <51148093+thaomike@users.noreply.github.com>
2020-10-17 21:14:30 +0700

    Updates

---
+++ precache-manifest.78d958746fec1c27acc57de98b0ff6f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58fe5711bd9aa06435bc78604fb9c08e",
+    "url": "/reatjs-todolist/index.html"
+  },
+  {
+    "revision": "634c5e8a471ed267465c",
+    "url": "/reatjs-todolist/static/css/main.b9e4e457.chunk.css"
+  },
+  {
+    "revision": "4d2126c74854721e02bd",
+    "url": "/reatjs-todolist/static/js/2.7e2fb384.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reatjs-todolist/static/js/2.7e2fb384.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "634c5e8a471ed267465c",
+    "url": "/reatjs-todolist/static/js/main.39469e3e.chunk.js"
+  },
+  {
+    "revision": "9e8c0748edfc9deee04b",
+    "url": "/reatjs-todolist/static/js/runtime-main.e2875dd2.js"
+  }
+]);
